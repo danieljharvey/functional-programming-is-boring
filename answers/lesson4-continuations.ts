0190034b4fd3f7ea90de96422aa975edee9ef3ed
Daniel Harvey <danieljamesharvey@gmail.com>
2020-10-01 09:00:50 +0100

    liftA2, liftA3

--- answers/lesson4-continuations.ts
+++ answers/lesson4-continuations.ts
@@ -48,6 +48,23 @@ export const bind = <A, B>(
     value.runContinuation(a => aToFB(a).runContinuation(b => next(b)))
   );
 
+// liftA2 :: (A -> B -> C) -> Continuation A -> Continuation B -> Continuation C
+export const liftA2 = <A, B, C>(
+  f: (a: A) => (b: B) => C,
+  contA: Continuation<A>,
+  contB: Continuation<B>
+): Continuation<C> => ap(map(f, contA), contB);
+
+// liftA3 :: (A -> B -> C -> D) -> Continuation A -> Continuation B -> Continuation C -> Continuation D
+export const liftA3 = <A, B, C, D>(
+  f: (a: A) => (b: B) => (c: C) => D,
+  contA: Continuation<A>,
+  contB: Continuation<B>,
+  contC: Continuation<C>
+): Continuation<D> => ap(ap(map(f, contA), contB), contC);
+
+// pair :: Continuation A -> Continuation B -> Continuation<[A,B]>
+
 // race :: Continuation A -> Continuation A -> Continuation A
 
 // delay :: Int -> Continuation A -> Continuation
