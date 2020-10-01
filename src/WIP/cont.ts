056903d67b756ed7b171a5edaba6d7ce39113b1c
Daniel Harvey <danieljamesharvey@gmail.com>
2020-10-01 07:48:02 +0100

    Continuation

--- src/WIP/cont.ts
+++ src/WIP/cont.ts
@@ -17,15 +17,18 @@ const continuation = <A>(
   runContinuation
 });
 
+// pure :: A -> Continuation A
 export const pure = <A>(a: A): Continuation<A> =>
   continuation(next => next(a));
 
+// map :: (A -> B) -> Continuation A -> Continuation B
 export const map = <A, B>(
   f: (a: A) => B,
   value: Continuation<A>
 ): Continuation<B> =>
   continuation(next => value.runContinuation(a => next(f(a))));
 
+// ap :: Continuation (A -> B) -> Continuation A -> Continuation B
 export const ap = <A, B>(
   fAB: Continuation<(a: A) => B>,
   a: Continuation<A>
@@ -35,3 +38,18 @@ export const ap = <A, B>(
       a.runContinuation((a: A) => next(f(a)))
     )
   );
+
+// bind :: (A -> Continuation B) -> Continuation A -> Continuation B
+export const bind = <A, B>(
+  aToFB: (a: A) => Continuation<B>,
+  value: Continuation<A>
+): Continuation<B> =>
+  continuation<B>(next =>
+    value.runContinuation(a => aToFB(a).runContinuation(b => next(b)))
+  );
+
+// race :: Continuation A -> Continuation A -> Continuation A
+
+// delay :: Int -> Continuation A -> Continuation
+
+// withTimeout :: Int -> A -> Continuation A -> Continuation A
