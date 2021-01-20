c34e0228b68953f0aa8421c3c8f5f429cad6c54e
Daniel Harvey <danieljamesharvey@gmail.com>
2021-01-20 09:42:08 +0000

    Tidy up Option exercises

--- src/lesson1-option.ts
+++ src/lesson1-option.ts
@@ -1,3 +1,4 @@
+// everything you could possibly need to know about a Horse
 export type Horse = {
   type: 'HORSE'
   name: string
@@ -5,6 +6,15 @@ export type Horse = {
   hasTail: boolean
 }
 
+// the type of a Horse that is pretty standard, all told
+type StandardHorse = {
+  name: string
+  hasTail: true
+  legs: 4
+  type: 'STANDARD_HORSE'
+}
+
+// our extensive dataset of two (2) horses
 export const standardHorses: Horse[] = [
   {
     type: 'HORSE',
@@ -20,9 +30,12 @@ export const standardHorses: Horse[] = [
   },
 ]
 
+// OK FUNCTIONS
+
+// our somewhat basic horse getting function
 const getHorse = (name: string) => {
   let found
-  standardHorses.forEach((standardHorse) => {
+  standardHorses.forEach(standardHorse => {
     if (standardHorse.name === name) {
       found = standardHorse
     }
@@ -30,20 +43,16 @@ const getHorse = (name: string) => {
   return found
 }
 
+// a function that improves the formatting of a horse's name
+// as it rightly deserves
 const tidyHorseName = (horse: Horse): Horse => {
   return {
     ...horse,
     name: horse.name.toLowerCase(),
   }
 }
-
-type StandardHorse = {
-  name: string
-  hasTail: true
-  legs: 4
-  type: 'STANDARD_HORSE'
-}
-
+// a validation function to check whether a Horse can also
+// be a StandardHorse
 const mandatoryTailCheck = (
   horse: Horse
 ): StandardHorse | undefined => {
@@ -58,6 +67,7 @@ const mandatoryTailCheck = (
   }
 }
 
+// a naive reference implementation of a horse finding function
 export const horseFinder = (name: string): string => {
   const horse = getHorse(name)
 
@@ -90,6 +100,14 @@ export const some = <A>(a: A): Option<A> => ({
 // none :: () -> Option never
 export const none = (): Option<never> => ({ type: 'None' })
 
+// FUNCTIONS THAT WE MIGHT WANT TO IMPLEMENT TO HELP US
+
+// isSome :: Option A -> Boolean
+export const isSome = undefined as any
+
+// isNone :: Option A -> Boolean
+export const isNone = undefined as any
+
 // map :: (A -> B) -> Option A -> Option B
 export const map = undefined as any
 
@@ -102,6 +120,8 @@ export const join = undefined as any
 // bind :: (A -> Option B) -> Option A -> Option B
 export const bind = undefined as any
 
+// THE NEW FUNCTIONS WE NEED TO WRITE
+
 // newGetHorse :: String -> Option<Horse>
 export const newGetHorse = undefined as any
 
@@ -109,13 +129,13 @@ export const newGetHorse = undefined as any
 export const newMandatoryTailCheck = undefined as any
 
 // newHorseFinder :: String -> String
-export const newHorseFinder  = (name: string): string => {
-  const horse = getHorse(name)
+export const newHorseFinder = (name: string): string => {
+  const horse = newGetHorse(name)
 
   const tidyHorse = horse ? tidyHorseName(horse) : undefined
 
   const standardHorse = tidyHorse
-    ? mandatoryTailCheck(tidyHorse)
+    ? newMandatoryTailCheck(tidyHorse)
     : undefined
 
   if (!standardHorse) {
--- src/tests/lesson1-option.test.ts
+++ src/tests/lesson1-option.test.ts
@@ -1,6 +1,9 @@
 import {
+  isSome,
+  isNone,
   some,
   none,
+  join,
   map,
   bind,
   orElse,
@@ -11,43 +14,59 @@ import {
 } from '../lesson1-option'
 
 describe('Option', () => {
-  it.skip('Some', () => {
-    expect(some(1)).toEqual({ type: 'Some', value: 1 })
+  describe.skip('Horse finding', () => {
+    it('newGetHorse', () => {
+      expect(newGetHorse('CHAMPION')).toEqual(some(standardHorses[0]))
+      expect(newGetHorse('CHOMPION')).toEqual(none())
+    })
+    it('newMandatoryTailCheck', () => {
+      const passHorse = standardHorses[1]
+      expect(newMandatoryTailCheck(passHorse).type).toEqual('Some')
+      const failHorse = standardHorses[0]
+      expect(newMandatoryTailCheck(failHorse)).toEqual(none())
+    })
+    it('newHorseFinder', () => {
+      expect(newHorseFinder('HOOVES_GALORE')).toEqual(
+        'Found a good horse named hooves_galore'
+      )
+      expect(newHorseFinder('CHOMPION')).toEqual(
+        'CHOMPION is not a good horse'
+      )
+    })
   })
-  it.skip('None', () => {
-    expect(none()).toEqual({ type: 'None' })
-  })
-  it.skip('Map', () => {
-    expect(map((a: number) => a + 1, none())).toEqual(none())
-    expect(map((a: number) => a + 1, some(1))).toEqual(some(2))
-  })
-  it.skip('Bind', () => {
-    expect(bind((a: number) => some(a + 1), none())).toEqual(
-      none()
-    )
-    expect(bind((a: number) => some(a + 1), some(1))).toEqual(some(2))
-    expect(bind((_: number) => none(), some(1))).toEqual(none())
-  })
-  it.skip('orElse', () => {
-    expect(orElse((a: number) => a + 1, 0, some(1))).toEqual(2)
-    expect(orElse((a: number) => a + 1, 0, none())).toEqual(0)
-  })
-  it.skip('newGetHorse', () => {
-    expect(newGetHorse('CHAMPION')).toEqual(some(standardHorses[0]))
-    expect(newGetHorse('CHOMPION')).toEqual(none())
-  })
-  it.skip('newMandatoryTailCheck', () => {
-    const passHorse = standardHorses[1]
-    expect(newMandatoryTailCheck(passHorse).type).toEqual('Some')
-    const failHorse = standardHorses[0]
-    expect(newMandatoryTailCheck(failHorse)).toEqual(none())
-  })
-  it.skip('newHorseFinder', () => {
-    expect(newHorseFinder('HOOVES_GALORE')).toEqual(
-      'Found a good horse named hooves_galore'
-    )
-    expect(newHorseFinder('CHOMPION')).toEqual(
-      'CHOMPION is not a good horse'
-    )
+  describe.skip('Helpers', () => {
+    it('Some', () => {
+      expect(some(1)).toEqual({ type: 'Some', value: 1 })
+    })
+    it('None', () => {
+      expect(none()).toEqual({ type: 'None' })
+    })
+    it('isSome', () => {
+      expect(isSome(some(1))).toBeTruthy()
+      expect(isSome(none())).toBeFalsy()
+    })
+    it('isNone', () => {
+      expect(isNone(some(1))).toBeFalsy()
+      expect(isNone(none())).toBeTruthy()
+    })
+    it('Join', () => {
+      expect(join(some(some(1)))).toEqual(some(1))
+      expect(join(some(none()))).toEqual(none())
+    })
+    it('Map', () => {
+      expect(map((a: number) => a + 1, none())).toEqual(none())
+      expect(map((a: number) => a + 1, some(1))).toEqual(some(2))
+    })
+    it('Bind', () => {
+      expect(bind((a: number) => some(a + 1), none())).toEqual(none())
+      expect(bind((a: number) => some(a + 1), some(1))).toEqual(
+        some(2)
+      )
+      expect(bind((_: number) => none(), some(1))).toEqual(none())
+    })
+    it('orElse', () => {
+      expect(orElse((a: number) => a + 1, 0, some(1))).toEqual(2)
+      expect(orElse((a: number) => a + 1, 0, none())).toEqual(0)
+    })
   })
 })
