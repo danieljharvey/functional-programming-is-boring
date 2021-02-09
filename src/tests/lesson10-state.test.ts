024e3428663dcc56ffd5538b6d8f1a6ba7791ed9
Daniel Harvey <danieljamesharvey@gmail.com>
2021-02-09 18:26:24 +0000

    State monad part 1

---
+++ src/tests/lesson10-state.test.ts
@@ -0,0 +1,79 @@
+import { pipe } from 'fp-ts/function'
+import * as S from 'fp-ts/State'
+import {
+  defaultStable,
+  addHorseToStable,
+  getHorses,
+  countHorses,
+  standardiseHorses,
+} from '../lesson10-state'
+
+describe('State', () => {
+  it('addHorseToStable works', () => {
+    const newState = S.execute(defaultStable)(
+      addHorseToStable({
+        horseName: 'Mr Horse',
+        age: 100,
+        hasTail: true,
+        numberOfLegs: 4,
+      })
+    )
+    expect(newState.horses.length).toBeGreaterThan(0)
+  })
+  describe('getHorses', () => {
+    it('gets zero horses', () => {
+      expect(S.evaluate(defaultStable)(getHorses())).toHaveLength(0)
+    })
+    it('gets one horse', () => {
+      expect(
+        S.evaluate(defaultStable)(
+          pipe(
+            addHorseToStable({
+              horseName: 'Mr Horse',
+              age: 100,
+              hasTail: true,
+              numberOfLegs: 4,
+            }),
+            S.chain(_ => getHorses())
+          )
+        )
+      ).toHaveLength(1)
+    })
+  })
+  describe('standardiseHorses', () => {
+    it('filtering zero horses leaves zero horses', () => {
+      expect(
+        S.evaluate(defaultStable)(
+          pipe(
+            standardiseHorses(),
+            S.chain(_ => countHorses())
+          )
+        )
+      ).toEqual(0)
+    })
+    it('gets one horse', () => {
+      expect(
+        S.evaluate(defaultStable)(
+          pipe(
+            addHorseToStable({
+              horseName: 'Mr Horse',
+              age: 100,
+              hasTail: true,
+              numberOfLegs: 3,
+            }),
+            S.chain(_ =>
+              addHorseToStable({
+                horseName: 'Mrs Horse',
+                age: 100,
+                hasTail: true,
+                numberOfLegs: 4,
+              })
+            ),
+            S.chain(_ => standardiseHorses()),
+            S.chain(_ => countHorses())
+          )
+        )
+      ).toEqual(1)
+    })
+  })
+})
