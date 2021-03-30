ee81d44e1a2dac41a28cbe28275dc5400d4e7909
Daniel Harvey <danieljamesharvey@gmail.com>
2021-03-30 14:38:03 +0100

    More state monad (#5)
    * 11 exercises

--- exercises/state.ts
+++ exercises/state.ts
@@ -1,5 +1,7 @@
 import * as S from 'fp-ts/State'
 import * as O from 'fp-ts/Option'
+import * as A from 'fp-ts/Apply'
+import { pipe } from 'fp-ts/function'
 
 // The state monad lets us store and update some state throughout a
 // computation.
@@ -48,3 +50,32 @@ export const seven = (): S.State<number[], void> => undefined as any
 
 // given a list of Horses, return all the Horses with an age of 10 or over
 export const eight = (): S.State<Horse[], Horse[]> => undefined as any
+
+// given two State values containing arrays, combine them, concatenating the
+// arrays
+// hint: take a look at SequenceT in 'fp-ts/Apply'
+export const nine = <S, A>(
+  a: S.State<S, A[]>,
+  b: S.State<S, A[]>
+): S.State<S, A[]> =>
+  pipe(
+    A.sequenceT(S.state)(a, b),
+    S.map(([a, b]) => [...a, ...b])
+  )
+
+// given a function that combines the values inside two State values, and two
+// state values, combine them to create a new State value
+// hint: search on the internet for the 'liftA2' function
+export const ten = <S, A, B, C>(
+  f: (a: A) => (b: B) => C,
+  a: S.State<S, A>,
+  b: S.State<S, B>
+): S.State<S, C> => pipe(a, S.map(f), S.ap(b))
+
+// given a value, and a function that changes state, make a state action that
+// returns the action and makes the state change
+export const eleven = <S, A>(a: A, fn: (s: S) => S): S.State<S, A> =>
+  pipe(
+    S.of<S, A>(a),
+    S.chainFirst(_ => S.modify(fn))
+  )
